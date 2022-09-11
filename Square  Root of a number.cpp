945d201ba4ecca4c51aba7238b6236314da19135
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2022-09-12 00:57:51 +0530

    Create Square  Root of a number.cpp

---
+++ Square  Root of a number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    float sq,n;
+
+    cout<<"Enter any positive number :: ";
+    cin>>n;
+
+    sq=sqrt(n);
+
+    cout<<"\nSquare  root of Entered Number [ "<<n<<" ] is :: "<<sq<<"\n";
+
+    return 0;
+}
