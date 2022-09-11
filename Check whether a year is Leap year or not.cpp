bbf4fd7d6e0708c025a9659e6c5887d17c38a7a8
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2022-09-12 01:00:48 +0530

    Create Check whether a year is Leap year or not.cpp

---
+++ Check whether a year is Leap year or not.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+
+    cout<<"Enter any Year (XXXX) :: ";
+    cin>>year;
+
+    if(year%100==0)
+    {
+        if(year%400==0)
+        {
+           cout<<"\nThe Entered Year [ "<<year<<" ] is a Leap Year.\n";
+        }
+
+    }
+    else
+    {
+        if(year%4==0)
+        {
+            cout<<"\nThe Entered Year [ "<<year<<" ] is a Leap Year.\n";
+        }
+        else
+        {
+            cout<<"\nThe Entered Year [ "<<year<<" ] is NOT a Leap Year.\n";
+        }
+
+    }
+
+   return 0;
+}
