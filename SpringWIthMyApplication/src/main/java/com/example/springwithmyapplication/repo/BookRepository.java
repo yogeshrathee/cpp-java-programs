b5d441f7705a30411d1f49553b3d76ce7d4db7db
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2022-10-10 15:55:23 +0530

    Delete SpringWIthMyApplication directory

--- SpringWIthMyApplication/src/main/java/com/example/springwithmyapplication/repo/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springwithmyapplication.repo;
-
-import com.example.springwithmyapplication.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book,Long> {
-
-
-}
