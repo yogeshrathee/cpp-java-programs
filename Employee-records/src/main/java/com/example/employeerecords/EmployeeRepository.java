881a996994605f80621784bd75c1475a26e4953d
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2022-10-10 18:15:56 +0530

    Add files via upload

---
+++ Employee-records/src/main/java/com/example/employeerecords/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.employeerecords;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee,Integer> {
+}
---
+++ Employee-records/src/test/java/com/example/employeerecords/EmployeeRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeerecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRecordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
