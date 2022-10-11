04a4bb8d5f4fd30f5802323673bd1b7b3d7695f4
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2022-10-12 01:23:43 +0530

    Delete employee-record-crud-api directory

--- employee-record-crud-api/Employee-records/src/main/java/com/example/employeerecords/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeerecords;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee,Integer> {
-    Employee findByName(String name);
-
-
-}
