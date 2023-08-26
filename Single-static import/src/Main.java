58b309512d246e6713c0c4746520ba575c7ada87
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-08-26 16:17:38 +0530

    Add files via upload

---
+++ Single-static import/src/Main.java
@@ -0,0 +1,12 @@
+//single-static import
+import static java.lang.Math.random;
+import static java.lang.Math.sqrt;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println(random());
+        System.out.println(sqrt(9));
+    }
+}
+
+
