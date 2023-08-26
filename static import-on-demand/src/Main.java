f1eebced2491256dbb4613f9cc1e96270aa05d4b
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-08-26 16:20:16 +0530

    Add files via upload

---
+++ static import-on-demand/src/Main.java
@@ -0,0 +1,11 @@
+//static import-on-demand
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+public class Main{
+    public static void main(String[] args){
+        out.println(random());
+        out.println(sqrt(9));
+
+    }
+}
