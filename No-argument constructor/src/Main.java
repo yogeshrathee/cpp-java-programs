213e278650c3f8346ece32f07e3d20362dd26e77
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-08-27 16:26:58 +0530

    Add files via upload

---
+++ No-argument constructor/src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    int i;
+    public Main(){  //No arguments constructor
+        System.out.println("Hyy , Yogesh");
+        System.out.println(i=10);
+    }
+    public static void main(String[] args){
+        Main obj=new Main();   // (create an object or reference for execute the constructor) or call the constructor
+        
+    }
+}
