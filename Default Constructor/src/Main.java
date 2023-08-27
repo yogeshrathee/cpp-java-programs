1feb3f07217ccaa69a29a4eea024d7755814833d
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-08-27 14:09:33 +0530

    Add files via upload

---
+++ Default Constructor/src/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    int a;
+    String b;
+    boolean c;
+    public static void main(String[] args){
+
+        Main obj=new Main();
+
+        System.out.println("default value of int : "+obj.a);
+        System.out.println("default value of String : "+obj.b);
+        System.out.println("default value of Boolean : "+obj.c);
+
+    }
+}
+//output::- 
+
+        //default value of int : 0
+        //default value of String : null
+        //default value of Boolean : false
